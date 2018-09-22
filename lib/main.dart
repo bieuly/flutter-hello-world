4dc070912bbcd87a44570cdbc4f03ff7876b4bc1
Billy Hoang <billy-hoang@hotmail.com>
2018-09-22 16:07:13 -0700

    install english words package dependency

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: AppBar(
+          title: Text("Billy's Flutter App"),
+        ),
+        body: new Center(
+          child: Text(wordPair.asPascalCase),
+        )
+      )
+    );
+  }
+}
